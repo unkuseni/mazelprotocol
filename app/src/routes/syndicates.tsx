372f2a68788719897ecd75aa8dc44cf86c980942
unkuseni <amudalatadeoye@gmail.com>
2026-02-10 13:29:20 +0100

    reset

--- app/src/routes/syndicates.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/syndicates")({
-  component: SyndicatesLayout,
-});
-
-function SyndicatesLayout() {
-  return <Outlet />;
-}
